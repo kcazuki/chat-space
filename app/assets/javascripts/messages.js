4bc06d953a52bf05604ed07515a7ece0a1c27c5a
kcazuki <53326300+kcazuki@users.noreply.github.com>
2019-08-09 11:31:01 +0900

    Update messages.js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -40,7 +40,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html)
-      $('.form__message').val('')
+      $('#new_message')[0].reset();
       $( ".form__submit").prop( "disabled", false );
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight},'fast');
     })
